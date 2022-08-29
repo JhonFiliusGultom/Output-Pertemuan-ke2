5966d66ca616cd37694a3eea12e308f3a1bb7da8
unknown <johnfliusgultom@gmail.com>
2022-08-29 19:51:59 +0700

    Tugas Pertemuan 2

---
+++ tugaspertemuan2.php
@@ -0,0 +1,28 @@
+<?php
+echo ("<h3>Tipe Data String </h3>");
+   $nama="Jhon Filius Gultom";  
+   $nim = "210411100019" ;
+   $kelas = "Pengembangan Aplikasi Web C" ; 
+   echo "Nama : $nama<br>";
+   echo "Nim  : $nim<br>";
+   echo "Kelas : $kelas<br>"
+?> 
+<?php 
+echo ("<h3>Tipe Data Float </h3>");
+$nilai1 = 7.1;
+$nilai2 = 9.7;
+$nilai3 = 8.3;
+#rata-rata
+$ratarata = ($nilai1 + $nilai2 + $nilai3) / 3;
+#Tampilkan data
+echo "Nilai 1: {$nilai1} <br>";
+echo "Nilai 2: {$nilai2} <br>";
+echo "Nilai 3: {$nilai3} <br>";
+echo "Rata-rata: {$ratarata} <br>";
+?>
+<?php 
+echo ("<h3>Tipe Data Integer </h3>");
+$nilai1 = 7.1;
+  $nilai = 10;
+  var_dump($nilai);
+?>
